ef44a2ae6c1e1d3f323faa8c512c64e1dcad5505
Dmitry16 <europartner@inbox.ru>
2019-09-07 08:07:49 +0300

    use_effect1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useForm from './useForm';
 
 const App = () => {
 
-  const [values, handleChange] = useForm({ email: 'qweeee@rty.ui', password: 'q' });
+  const [values, handleChange] = useForm({ 
+    email: 'qweeee@rty.ui',
+    firstname: 'bobbie',
+    password: 'q'
+  });
+
+  useEffect(() => {
+    console.log('renderrrrr!');
+  }, [values.email, values.password]);
 
   return (
     <div>
       <input name='email' value={values.email}
+        onChange={ handleChange }>
+      </input>
+      <input name='firstname' value={values.firstname}
         onChange={ handleChange }>
       </input>
       <input name='password' type='password' value={values.password}
